4b91eb40f508144fc4899ba2e4f711ecf840b87f
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2022-04-11 12:05:11 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Postman from './components/Postman';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Newpostman from './components/Newpostman';
 
 ReactDOM.render(
   <React.StrictMode>
